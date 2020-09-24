237532de55071f8c62818c178d6f2b0334c308df
Honghao Huang (1000090458) <HUANH8@student.op.ac.nz>
2020-09-24 15:35:45 +1200

    The second cod by Evan

--- GroupC/GroupC/Program.cs
+++ GroupC/GroupC/Program.cs
@@ -7,6 +7,7 @@ namespace GroupC
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Group!!")
             Console.ReadLine();
         }
     }
