9a848b0104e4a1dd2054621650dcd9cb39eafed9
Stephen Parker (1000003363) <PARKSA2@student.op.ac.nz>
2020-10-13 14:21:36 +1300

    added main menu and method 3

--- GroupC/GroupC/Program.cs
+++ GroupC/GroupC/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace GroupC
 {
@@ -24,10 +25,11 @@ namespace GroupC
         //method3
         static void method3()
         {
-
-
-
-
+            Console.Clear();
+            Console.WriteLine("empty for now");
+            
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
         }
 
 
@@ -37,8 +39,41 @@ namespace GroupC
 
         static void Main(string[] args)
         {
+            int selection;
+            string[] title = { "method1", "method2", "method3", "Exit" };
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Main Menu".PadLeft(20));
+                Console.WriteLine("Please Select a number".PadLeft(28));
+                for (int i = 0; i < title.Length; i++)
+                {
+                    Console.Write((i +1).ToString().PadLeft(7).PadRight(11));
+                    Console.WriteLine(title[i]);
+                }
+                // selection to call out task or exit the program
+                selection = Convert.ToInt32(Console.ReadLine());
+                
+                switch (selection)
+                {
+                    case 1:
+                        method1();
+                        break;
 
-          
+                    case 2:
+                        method2();
+                        break;
+
+                    case 3:
+                        method3();
+                        break;
+
+
+                    default:
+                        break;
+                }
+            } while (selection != 4);
+
         }
     }
 }
